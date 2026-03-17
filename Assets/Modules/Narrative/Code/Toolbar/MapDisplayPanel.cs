8f4fd4d745dd8b332a0be3a2d730dcd461235a47
soliloquymoon <soliloquy.moon@gmail.com>
2026-03-17 16:54:02 -0500

    Implement map travel UI with hub and location selection

--- Assets/Modules/Narrative/Code/Toolbar/MapDisplayPanel.cs
+++ Assets/Modules/Narrative/Code/Toolbar/MapDisplayPanel.cs
@@ -5,62 +5,46 @@ using FieldDay.UI;
 using UnityEngine;
 using UnityEngine.UI;
 using BeauUtil.UI;
-using System.Collections.Generic;
 
-namespace AIS.Narrative {
-    public sealed class MapDisplayPanel : SharedPanel {
-        private enum MapMode {
+namespace AIS.Narrative
+{
+    public sealed class MapDisplayPanel : SharedPanel
+    {
+        private enum MapMode
+        {
             Travel, // move between locations
             Model // view the invasion model
         }
-
-        [System.Serializable]
-        public class Hub : Location
-        {
-            public List<Location> locations = new List<Location>();
-            // public List<Path> paths = new List<Path>();
-            public Vector3 cameraTransform;
-        }
-
-        [System.Serializable]
-        public class Location
-        {
-            public string locationName;
-            public Vector2 position = new Vector2();
-        }
-
-        // [System.Serializable]
-        // public class Path
-        // {
-        //     public Location location1, location2;
-        //     public float travelTime;
-        // }
-
-        private Hub currentHub;
-        private Location currentLocation;
-        public List<Hub> hubs;
-
         private MapMode? currMode = null;
         public Button travelButton, modelButton;
-        public GameObject travelPointContainer, pathContainer;
-        public GameObject travelPointPrefab;
 
-        protected override void Awake() {
+
+        public GameObject travelPointsContainer;
+        public TravelPointsDisplay hubSelectionDisplay;
+        public TravelPointsDisplay[] travelPointsDisplays;
+        private int currentHubIdx;
+        private int selectedHubIdx;
+
+        protected override void Awake()
+        {
             base.Awake();
             Hide();
 
             travelButton.onClick.AddListener(() => SwitchMapMode(MapMode.Travel));
             modelButton.onClick.AddListener(() => SwitchMapMode(MapMode.Model));
+            currentHubIdx = 0;
         }
 
-        public override void Show() {
+        public override void Show()
+        {
             base.Show();
             Game.Gui.PushPriority(m_InputLayer);
 
             SwitchMapMode(MapMode.Travel); // default
         }
 
-        public override void Hide() {
+        public override void Hide()
+        {
             Game.Gui.PopPriority(m_InputLayer);
             base.Hide();
 
@@ -68,7 +52,8 @@ namespace AIS.Narrative {
                 SwitchMapMode(MapMode.Model); // enable all icons in InvasionModel before closing
         }
 
-        private void SwitchMapMode(MapMode newMode) {
+        private void SwitchMapMode(MapMode newMode)
+        {
             if (currMode == newMode) return;
 
             currMode = newMode;
@@ -77,69 +62,72 @@ namespace AIS.Narrative {
             Transform container = InvasionModel.Instance.gameObject.transform.GetChild(1);
             bool isTravelMode = newMode == MapMode.Travel;
 
-            for (int i = 0; i < container.childCount; i++) {
+            for (int i = 0; i < container.childCount; i++)
+            {
                 GameObject child = container.GetChild(i).gameObject;
                 // Travel mode: Hide icons
                 // Model mode: Show icons
-                if (child.name.StartsWith("Pathway")) {
+                if (child.name.StartsWith("Pathway"))
+                {
                     child.transform.GetChild(1).gameObject.SetActive(!isTravelMode);
                     child.transform.GetChild(2).gameObject.SetActive(!isTravelMode);
                 }
-                else if (child.name.StartsWith("Species")) {
+                else if (child.name.StartsWith("Species"))
+                {
                     child.SetActive(!isTravelMode);
                 }
             }
 
-            travelPointContainer.SetActive(isTravelMode);
-            SetHubPoints();
+            travelPointsContainer.SetActive(isTravelMode);
+            if (isTravelMode)
+            {
+                selectedHubIdx = currentHubIdx;
+                TravelToSelectedHub();
+            }
+            else
+            {
+                // Zoom out
+                Camera.main.transform.position = hubSelectionDisplay.cameraTransform;
+                Camera.main.orthographicSize = 5f;
+            }
 
             // TODO: current code is temporary -- implement proper animation later
             travelButton.GetComponent<RoundedRectGraphic>().color = isTravelMode ? Color.white : Color.gray;
             modelButton.GetComponent<RoundedRectGraphic>().color = isTravelMode ? Color.gray : Color.white;
         }
 
-        public void SetHubPoints()
+        public void ShowHubSelectionPanel()
         {
-            DestroyAllTravelPoints();
-            foreach(Hub hub in hubs) {
-                GameObject travelPoint = Instantiate(travelPointPrefab, travelPointContainer.transform);
-                travelPoint.GetComponent<Button>().onClick.AddListener(() => SelectHub(hub));
-                travelPoint.GetComponent<RectTransform>().anchoredPosition = hub.position;
-                travelPoint.GetComponent<CursorHint>().TooltipHeader = hub.locationName;
-            }
-
             // Zoom out
-            Camera.main.transform.position = new Vector3(-1, 0, -10);
+            Camera.main.transform.position = hubSelectionDisplay.cameraTransform;
             Camera.main.orthographicSize = 5f;
+
+            travelPointsDisplays[currentHubIdx].gameObject.SetActive(false);
+            hubSelectionDisplay.gameObject.SetActive(true);
+
+            hubSelectionDisplay.SetCurrentLocation(currentHubIdx);
         }
 
-        public void SelectHub(Hub hub) {
-            Debug.Log($"[MapDisplayPanel] Selected hub: {hub.locationName}");
-            currentHub = hub;
-            currentLocation = hub.locations[0];
+        public void SelectHub(int index)
+        {
+            selectedHubIdx = index;
 
-            DestroyAllTravelPoints();
-            foreach(Location loc in hub.locations) {
-                GameObject travelPoint = Instantiate(travelPointPrefab, travelPointContainer.transform);
-                travelPoint.GetComponent<Button>().onClick.AddListener(() => SelectLocation(loc));
-                travelPoint.GetComponent<RectTransform>().anchoredPosition = loc.position;
-                travelPoint.GetComponent<CursorHint>().TooltipHeader = loc.locationName;
-            }
+            if (currentHubIdx == index)
+                TravelToSelectedHub();
+            else
+                hubSelectionDisplay.SelectLocation(index);
+        }
 
+        public void TravelToSelectedHub()
+        {
             // Zoom in
-            Camera.main.transform.position = hub.cameraTransform;
+            Camera.main.transform.position = travelPointsDisplays[selectedHubIdx].cameraTransform;
             Camera.main.orthographicSize = 2.5f;
-        }
 
-        public void SelectLocation(Location loc) {
-            currentLocation = loc;
-            Debug.Log($"[MapDisplayPanel] Selected location: {loc.locationName}");
+            currentHubIdx = selectedHubIdx;
+            hubSelectionDisplay.gameObject.SetActive(false);
+            hubSelectionDisplay.TravelToSelectedLocation();
+            travelPointsDisplays[currentHubIdx].gameObject.SetActive(true);
         }
-
-        private void DestroyAllTravelPoints() {
-            foreach (Transform child in travelPointContainer.transform)
-                Destroy(child.gameObject);
-        }
-
     }
 }
