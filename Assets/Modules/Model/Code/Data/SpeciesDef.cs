d319954e01d516e8799ca8518b3c3ad77696e37b
Alice Zhou <qzhou223@wisc.edu>
2026-04-10 14:02:53 -0500

    revision of the model

--- Assets/Modules/Model/Code/Data/SpeciesDef.cs
+++ Assets/Modules/Model/Code/Data/SpeciesDef.cs
@@ -11,5 +11,6 @@ namespace AIS.Model
         public SerializedHash32 SpeciesId;
         public Sprite Sprite;
         public int numEggs;
+        // reproduction condition
     }
 }
--- Assets/Modules/Model/Code/Pathway.cs
+++ Assets/Modules/Model/Code/Pathway.cs
@@ -30,13 +30,13 @@ namespace AIS.Model
     public enum PathwayType
     {
         Currents = 0x01,
-        // Upstream = 0x01
-        // Downstream = 0x02 (need to change all the followings)
         PetTrade = 0x02,
         BoatHulls = 0x04,
         BaitBuckets = 0x08,
         BallastWater = 0x10,
         Aquarium = 0x20,
+        // Upstream = 0x40,
+        // Downstream = 0x80,
     }
 
     public enum RateType
