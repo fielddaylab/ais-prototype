a79d617862ea9b75532e27ca771b4e4c137c113b
BeauchesneFieldDay <beauchesne@wisc.edu>
2026-04-17 13:57:51 -0500

    * added missing Lamprey-Discovered card

--- Assets/Modules/Narrative/Code/State/EvidenceCard.cs
+++ Assets/Modules/Narrative/Code/State/EvidenceCard.cs
@@ -5,6 +5,5 @@ namespace AIS.Narrative {
     [CreateAssetMenu(menuName ="Narrative/Evidence Card")]
     public sealed class EvidenceCard : NamedAsset {
         public string Label;
-                public int id;
     }
 }
