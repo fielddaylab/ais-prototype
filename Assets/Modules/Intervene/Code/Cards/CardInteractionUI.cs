6141472d19c89ab2dc403cd820ceb54bdbd30c10
REDY-a <alice.zqr233@gmail.com>
2026-07-22 19:08:24 +0800

    bind spend budget behavior to action effect UI buttons instead of card interaction ui

--- Assets/Modules/Intervene/Code/Cards/CardInteractionUI.cs
+++ Assets/Modules/Intervene/Code/Cards/CardInteractionUI.cs
@@ -55,14 +55,14 @@ namespace AIS.Intervene
                 return; 
             }
 
-            if (Game.SharedState.TryGet(out ActionCardsState cardsState))
-            {
-                if (cardsState.AllActionCards.TryGetValue(selectedCards[0].CardID, out ActionCardData data))
-                {
-                    BudgetUtility.Spend(InterveneBudgetInterfacer.Instance, data.Cost);
-                }
-            }
-            CardInteractionMgr.Instance.Hand.ClearSelections();
+            //if (Game.SharedState.TryGet(out ActionCardsState cardsState))
+            //{
+            //    if (cardsState.AllActionCards.TryGetValue(selectedCards[0].CardID, out ActionCardData data))
+            //    {
+            //        BudgetUtility.Spend(InterveneBudgetInterfacer.Instance, data.Cost);
+            //    }
+            //}
+            //CardInteractionMgr.Instance.Hand.ClearSelections();
             UseSelectedBtn.interactable = InterveneBudgetInterfacer.Instance.WorkingBudget.Budget > 0 ? true : false;
 
             AisGame.Events.Dispatch(InterveneEvents.OnEffectSpecifyBegin);
